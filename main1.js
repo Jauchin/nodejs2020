04ab547d9fe286eb3cd9b97d08d3940bd663e15a
JauChin <jau.chin.shieh@gmail.com>
2020-07-30 14:09:17 +0800

    modify user bug

--- main1.js
+++ main1.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
-let port = process.env.PORT || 8888;
-let user_router = require('./routers/main_router.js');
+let port = process.env.PORT || 8888
+let user_router = require('./routers/main_router.js')
 
 app.get("/", (req, res) => {
     res.send("This is main page.");
@@ -11,7 +11,7 @@ app.get("/what", (req, res) => {
     res.send("This is what page.");
 });
 
-app.use("/user", user_router);
+app.use("/user", user_router)
 
 app.listen(port, () => {
     console.log(`server listen on port ${port}`)
