a2f696cb83de8702a38ea4d77fdfa240e1f4ecfe
JauChin <jau.chin.shieh@gmail.com>
2020-07-30 11:41:16 +0800

    add picsum photo

--- routers/Fisheep.js
+++ routers/Fisheep.js
@@ -22,7 +22,7 @@ router.get('/about', function(req, res) {
   res.send(
     '<h1> About Fisheep </h1>'+
     // '\n <img src="https://www.omgtw.com/upload/article/original/be134fac90625546754962aeef34925c.jpg" height="60%">'
-    '\n <img src="https://i.picsum.photos/id/100/2500/1656.jpg" >'    
+    '\n <img src="https://i.picsum.photos/id/100/2500/1656.jpg?hmac=gWyN-7ZB32rkAjMhKXQgdHOIBRHyTSgzuOK6U0vXb1w" height="60%" >'    
     );
 });
 
